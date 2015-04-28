c3cb6b0f11084549103e04b73b34b2105a8978d6
koko <koko@Koko>
2015-04-28 13:36:54 +0700

    ugvguvytvyt

---
+++ src/perkalian.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/* IMPORT UTILITI SCANNER PADA APLIKASI JAVA
+/**
+*
+* @author Bombercomputer
+*/
+public class perkalian {
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+// INPUT CODE DI BAWAH SINI
+
+Scanner masuk=new Scanner(System.in);
+System.out.print("NILAI PERTAMA:");
+int N1= masuk.nextInt();
+
+System.out.print("NILAI KEDUA:");
+int N2= masuk.nextInt();
+
+int hasil=N1*N2;
+System.out.print ("hasil total=" +hasil);
+     }
+
+}
+
